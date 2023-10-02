d66d33b7bcba4481cef8699176df5de64edb05ba
Max Henry <mhenry@eers.ca>
2023-10-01 23:13:33 -0400

    Add patch reconstructor

--- src/chatgpt_util_imports.py
+++ src/chatgpt_util_imports.py
@@ -82,3 +82,86 @@ def generate_dataset(fpath):
     dataset = [unpack_patch(patch) for patch in patches]
 
     return dataset
+
+
+def pack_patch(parameters):
+    packed_data = bytearray(128)  # Initialize with 128 zeros
+
+    def write_byte(offset, value):
+        packed_data[offset] = value & 0x7F
+
+    def pack_byte(high_nibble, low_nibble):
+        return ((high_nibble << 4) & 0xF0) | (low_nibble & 0x0F)
+
+    # Pack the operators
+    operators = ["OP6", "OP5", "OP4", "OP3", "OP2", "OP1"]
+    for idx, op in enumerate(operators):
+        base_offset = idx * 17
+        write_byte(base_offset, parameters[f"{op} EG rate 1"])
+        write_byte(base_offset + 1, parameters[f"{op} EG rate 2"])
+        write_byte(base_offset + 2, parameters[f"{op} EG rate 3"])
+        write_byte(base_offset + 3, parameters[f"{op} EG rate 4"])
+        write_byte(base_offset + 4, parameters[f"{op} EG level 1"])
+        write_byte(base_offset + 5, parameters[f"{op} EG level 2"])
+        write_byte(base_offset + 6, parameters[f"{op} EG level 3"])
+        write_byte(base_offset + 7, parameters[f"{op} EG level 4"])
+        write_byte(base_offset + 8, parameters[f"{op} KBD LEV SCL BRK PT"])
+        write_byte(base_offset + 9, parameters[f"{op} KBD LEV SCL LFT DEPTH"])
+        write_byte(base_offset + 10, parameters[f"{op} KBD LEV SCL RHT DEPTH"])
+        packed_data[base_offset + 11] = pack_byte(parameters[f"{op} KBD LEV SCL RHT CURVE"], parameters[f"{op} KBD LEV SCL LFT CURVE"])
+        packed_data[base_offset + 12] = pack_byte(parameters[f"{op} OSC DETUNE"], parameters[f"{op} KBD RATE SCALING"])
+        packed_data[base_offset + 13] = pack_byte(parameters[f"{op} KEY VEL SENSITIVITY"], parameters[f"{op} AMP MOD SENSITIVITY"])
+        write_byte(base_offset + 14, parameters[f"{op} OPERATOR OUTPUT LEVEL"])
+        packed_data[base_offset + 15] = ((parameters[f"{op} OSC MODE"] << 6) & 0x40) | (parameters[f"{op} OSC FREQ COARSE"] & 0x3F)
+        write_byte(base_offset + 16, parameters[f"{op} OSC FREQ FINE"])
+
+    # After the operators, pack the rest of the parameters
+    write_byte(102, parameters["PITCH EG rate 1"])
+    write_byte(103, parameters["PITCH EG rate 2"])
+    write_byte(104, parameters["PITCH EG rate 3"])
+    write_byte(105, parameters["PITCH EG rate 4"])
+    write_byte(106, parameters["PITCH EG level 1"])
+    write_byte(107, parameters["PITCH EG level 2"])
+    write_byte(108, parameters["PITCH EG level 3"])
+    write_byte(109, parameters["PITCH EG level 4"])
+    packed_data[110] = parameters["ALGORITHM #"] & 0x3F
+    packed_data[111] = pack_byte(parameters["OSCILLATOR SYNC"], parameters["FEEDBACK"])
+    write_byte(112, parameters["LFO SPEED"])
+    write_byte(113, parameters["LFO DELAY"])
+    write_byte(114, parameters["LFO PITCH MOD DEPTH"])
+    write_byte(115, parameters["LFO AMP MOD DEPTH"])
+    packed_data[116] = ((parameters["PITCH MOD SENSITIVITY"] << 4) & 0x70) | (parameters["LFO WAVEFORM"] << 1) | parameters["LFO SYNC"]
+    write_byte(117, parameters["TRANSPOSE"])
+
+    # Handle the voice name
+    voice_name = parameters.get("VOICE NAME", "")  # Fetch the name or use an empty string if it's missing
+    voice_name_padded = voice_name.ljust(10)  # Pad with spaces to make it 10 characters
+    for i in range(10):
+        write_byte(118 + i, ord(voice_name_padded[i]))
+
+    return packed_data
+
+# Generating the SysEx message for 32 patches
+def generate_sysex_with_corrected_checksum(data):
+    # Create a SysEx message for 32 patches
+    sysex_start = bytes([0xF0, 0x43, 0x00, 0x09, 0x20, 0x00])
+    checksum = (128 - sum(data) % 128) % 128
+    sysex_end = bytes([checksum, 0xF7])
+
+    return sysex_start + data + sysex_end
+
+def write_sysex_to_file(sysex_data, filename):
+    with open(filename, 'wb') as sysex_file:
+        sysex_file.write(sysex_data)
+
+def example(dataset):
+    # Pack the first 32 patches
+    packed_patches = bytearray()
+    for patch in dataset[:32]:
+        packed_patches += pack_patch(patch)
+
+    # Generate the SysEx message for 32 patches
+    sysex_data_32_patches = generate_sysex_with_corrected_checksum(packed_patches)
+
+    # Example usage
+    write_sysex_to_file(sysex_data_32_patches, "output_filename.syx")
